b8c7c97552fe83aedb6d1e2f448c22ff943cebcc
paravozz <prvz-work@outlook.com>
2020-09-13 19:04:04 +0300

    Updates

---
+++ precache-manifest.b3909b0766dc4f7fd12cdc9b925cb314.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e3c41e476e74e3a5073851c0324849",
+    "url": "/wezom-test/index.html"
+  },
+  {
+    "revision": "efe8a0eb9ef00b07b729",
+    "url": "/wezom-test/static/css/2.5c600ed6.chunk.css"
+  },
+  {
+    "revision": "aafdfa49bc5241e393ba",
+    "url": "/wezom-test/static/css/main.a0064841.chunk.css"
+  },
+  {
+    "revision": "efe8a0eb9ef00b07b729",
+    "url": "/wezom-test/static/js/2.97e93fe4.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/wezom-test/static/js/2.97e93fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafdfa49bc5241e393ba",
+    "url": "/wezom-test/static/js/main.f26e4ba5.chunk.js"
+  },
+  {
+    "revision": "05b450423a4384e4c199",
+    "url": "/wezom-test/static/js/runtime-main.75376a96.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/wezom-test/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/wezom-test/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/wezom-test/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
