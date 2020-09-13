8a0a045df95691a6ae6bbe025c69a5f3a1bc6b84
paravozz <prvz-work@outlook.com>
2020-09-13 19:14:33 +0300

    gh-pages update

--- src/routes/history.js
+++ src/routes/history.js
@@ -1,6 +1,6 @@
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { HISTORY_BASENAME } from 'constants/env';
 
-const history = createBrowserHistory({ basename: HISTORY_BASENAME });
+const history = createHashHistory({ basename: HISTORY_BASENAME });
 
 export { history };
